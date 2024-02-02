7707926fc0796d8f1d07a6a24a3b40ffb473336b
Dima <dimasalabay0711@gmail.com>
2024-02-02 13:13:18 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import MovieList from './MovieList/MovieList';
+
+// import MainMenu from './MainMenu/MainManu';
+
+// import HomePage from 'pages/HomePage/HomePage';
+// import PostsSearchPage from 'pages/PostsSsearchPage/PostsSsearchPage';
+// import Contacts from 'pages/ContactsPage/ContactsPage';
+// import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+// import SinglePostPage from 'pages/SinglePostPage/SinglePostPage';
+
 export const App = () => {
-  return <p>Hello</p>;
+  return (
+    <div className="app">
+      <MovieList />;
+      {/* <MainMenu />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posts-search" element={<PostsSearchPage />} />
+        <Route path="/posts/:id" element={<SinglePostPage />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes> */}
+    </div>
+  );
 };
